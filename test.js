88c47de4c2d631370c0cec937254da7740083303
Elia Kuzmanova <eliamitova7@gmail.com>
2023-01-24 13:17:43 +0100

    make model for the cats collection

--- test.js
+++ test.js
@@ -1,10 +1,25 @@
 const mongoose = require('mongoose');
 
 async function main() {
+
 mongoose.set('strictQuery', false)
  await mongoose.connect('mongodb://127.0.0.1:27017/test');
  console.log("Database connected!");
+
 }
 
+const catsSchema = new mongoose.Schema({
+    "name": {
+        "type":String,
+        "required": true
+    },
+    "age": Number,
+    "breed": {
+        "type" : String,
+        "enum": ["EKH", "British", "Persian", "Angora"]
+    }
+})
+
+const Cat = mongoose.model("Cat", catsSchema)
 
 main();
