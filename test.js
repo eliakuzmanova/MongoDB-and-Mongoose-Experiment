9f0426a606207a0127a29faf21a765abd9938123
Elia Kuzmanova <eliamitova7@gmail.com>
2023-01-24 13:39:28 +0100

    add find function

--- test.js
+++ test.js
@@ -1,10 +1,11 @@
+
 const mongoose = require('mongoose');
 const Cat = require('./models/Cat');
 
 async function main() {
 
 mongoose.set('strictQuery', false)
- await mongoose.connect('mongodb://127.0.0.1:27017/test');
+ await mongoose.connect('mongodb://127.0.0.1:27017/catShelter');
  console.log("Database connected!");
 
 }
@@ -20,8 +21,16 @@ async function saveCat(name) {
     await newCat.save();
     const data = await Cat.find({name});
     console.log(data);
+    console.log("Just saved!");
 }
 
+async function findCat(name) {
+    const data = await Cat.find({name});
+    console.log(data);
+    console.log("Just fined!");
+
+}
+    //  findCat("Tschar")
 saveCat(newCat.name)
 
 main();
